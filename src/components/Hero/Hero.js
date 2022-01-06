5d465709d0e0869cab02858ba3aad77dbda39dd8
Amirmazed <76693565+Amirmazed@users.noreply.github.com>
2022-01-06 21:20:47 +0600

    change cv

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -16,7 +16,7 @@ const Hero = (props) => (
       Hi, I'm Amir Mazed. 
       Full Stack Web Developer. Experienced in Javascript, React, Node.js, Express.js, Mongodb
       </SectionText>
-      <Button onClick={() => window.location ='https://drive.google.com/file/d/1g3Eant_ExWU37iHpX76Zg9va0Hsb70CX/view?fbclid=IwAR1R3XE-mLx7ZR3gYcUGrZcrMVFaSvHCDKuE1LXyO11k0E-er52zSyQrXOM'}>Learn More</Button>
+      <Button onClick={() => window.location ='https://drive.google.com/file/d/1HTz8c6rEzDWYQ4XW7G1KCw716tCCYeb9/view?usp=sharing'}>Learn More</Button>
     </LeftSection>
   </Section>
 
